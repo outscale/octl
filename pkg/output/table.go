043fa805a708902a5ffc03c0fb709a65e9678d2a
Jean-François Bustarret <jean-francois.bustarret@outscale.com>
2026-02-10 16:56:19 +0000

    🚸 ux(tables): add colors to states

--- pkg/output/table.go
+++ pkg/output/table.go
@@ -16,6 +16,41 @@ import (
 	"github.com/samber/lo"
 )
 
+var (
+	green  = lipgloss.NewStyle().Foreground(lipgloss.Color("113"))
+	yellow = lipgloss.NewStyle().Foreground(lipgloss.Color("184"))
+	red    = lipgloss.NewStyle().Foreground(lipgloss.Color("202"))
+	colors = map[string]map[string]lipgloss.Style{
+		"State": map[string]lipgloss.Style{
+			"running":   green,
+			"available": green,
+			"ACTIVE":    green,
+			"InService": green,
+			"attached":  green,
+
+			"pending":       yellow,
+			"stopping":      yellow,
+			"stopped":       yellow,
+			"deleting":      yellow,
+			"in-use":        yellow,
+			"INACTIVE":      yellow,
+			"disabled":      yellow,
+			"confirming":    yellow,
+			"allocated":     yellow,
+			"attaching":     yellow,
+			"detaching":     yellow,
+			"shutting-down": yellow,
+
+			"deleted":      red,
+			"OutOfService": red,
+			"rejected":     red,
+			"expired":      red,
+			"failed":       red,
+			"terminated":   red,
+		},
+	}
+)
+
 type Table struct {
 	Content string
 	Columns []config.Column
@@ -33,6 +68,18 @@ func (t Table) Output(ctx context.Context, v any) error {
 		}
 	} else {
 		rows = append(rows, GetRow(vv, t.Columns))
+	}
+	for r := range rows {
+		for c := range rows[r] {
+			styles, found := colors[headers[c]]
+			if !found {
+				continue
+			}
+			vstyle, found := styles[rows[r][c]]
+			if found {
+				rows[r][c] = vstyle.Render(rows[r][c])
+			}
+		}
 	}
 	cellStyle := lipgloss.NewStyle().Padding(0, 1)
 	headerStyle := lipgloss.NewStyle().Bold(true).Align(lipgloss.Center)
